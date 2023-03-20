4379bfe11d82ce74871f418a75556c8f5f470a0f
Noah Lewis <47840925+TheAssembler1@users.noreply.github.com>
2023-03-19 22:27:27 -0600

    added test for subscriptions

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,16 +2,60 @@ use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
-    let port = spawn_app();
+    let uri = spawn_app();
     let client = reqwest::Client::new();
     let response = client
-        .get(&format!("{}/health_check", &port))
+        .get(&format!("{}/health_check", &uri))
         .send()
         .await
         .expect("Failed to execute request.");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let uri = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &uri))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let uri = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le@20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing the email and name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &uri))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}",
+            error_message
+        );
+    }
 }
 
 fn spawn_app() -> String {
